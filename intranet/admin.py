9454d8de468a316ee9c689af79b924e3cb7159e9
alazo <alain.zosso@webzos.com>
2018-07-29 11:40:40 +0200

    Intranet app

---
+++ intranet/admin.py
@@ -0,0 +1,32 @@
+
+import os
+
+from django.conf import settings
+from django.contrib import admin
+
+from intranet.models import IntranetDoc
+
+# Register your models here.
+
+
+class IntranetDocAdmin(admin.ModelAdmin):
+
+    list_display = ('__str__', 'module', 'authorization')
+
+    def save_model(self, request, obj, form, change):
+        searched_file = 'intranet/{0}'.format(form.cleaned_data['doc'])
+        try:
+            doc = IntranetDoc.objects.get(doc=searched_file)
+            form.save()
+            # Override previous file
+            filename = os.path.join(settings.MEDIA_ROOT, searched_file)
+            file = request.FILES['doc']
+            with open(filename, 'wb+') as destination:
+                for chunk in file.chunks():
+                    destination.write(chunk)
+
+        except IntranetDoc.DoesNotExist:
+            super(IntranetDocAdmin, self).save_model(request, obj, form, change)
+
+
+admin.site.register(IntranetDoc, IntranetDocAdmin)
